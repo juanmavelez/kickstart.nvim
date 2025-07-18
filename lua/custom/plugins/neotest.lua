52dfe77a77c67191aa08016186a4bd7cc79c02ff
juan manuel velez <juanmanuelveleztorres@gmail.com>
2025-07-18 11:02:28 +0200

    save

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -7,8 +7,54 @@ return {
     'nvim-treesitter/nvim-treesitter',
     'marilari88/neotest-vitest', -- vitest
   },
+  keys = {
+    {
+      '<leader>nr',
+      function()
+        require('neotest').run.run()
+      end,
+      desc = 'Run nearest test',
+    },
+    {
+      '<leader>nf',
+      function()
+        require('neotest').run.run(vim.fn.expand '%')
+      end,
+      desc = 'Run current file',
+    },
+    {
+      '<leader>na',
+      function()
+        require('neotest').run.run { suite = true }
+      end,
+      desc = 'Run all tests',
+    },
+    {
+      '<leader>no',
+      function()
+        require('neotest').output.open()
+      end,
+      desc = 'Show test output',
+    },
+    {
+      '<leader>np',
+      function()
+        require('neotest').output_panel.toggle()
+      end,
+      desc = 'Toggle output panel',
+    },
+    {
+      '<leader>nv',
+      function()
+        require('neotest').summary.toggle()
+      end,
+      desc = 'Toggle summary',
+    },
+  },
   config = function()
-    require('neotest').setup {
+    local neotest = require 'neotest'
+
+    neotest.setup {
       adapters = {
         require 'neotest-vitest' {
           filter_dir = function(name, rel_path, root)
@@ -17,5 +63,13 @@ return {
         },
       },
     }
+
+    -- Auto-open output panel after test run
+    neotest.listeners = neotest.listeners or {}
+    neotest.listeners.results = function(_, results)
+      if results and next(results) ~= nil then
+        neotest.summary.open()
+      end
+    end
   end,
 }
