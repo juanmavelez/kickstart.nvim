5744bc72369ffaeedc8458bde256d4b22df4b26f
werte <juanmanuelveleztorres@gmail.com>
2025-07-24 02:24:42 +0200

    improve config, add git status

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,6 +12,7 @@ return {
   lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { 'gs', ':Neotree float git_status<CR>', desc = '[G]it [S]tatus' },
   },
   opts = {
     filesystem = {
@@ -22,4 +23,34 @@ return {
       },
     },
   },
+  config = function()
+    require('neo-tree').setup {
+      nable_git_status = true,
+      sources = { 'filesystem', 'git_status' },
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            -- Change type
+            added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
+            deleted = '✖', -- this can only be used in the git_status source
+            renamed = '󰁕', -- this can only be used in the git_status source
+            -- Status type
+            untracked = '',
+            ignored = '',
+            unstaged = '󰄱',
+            staged = '',
+            conflict = '',
+          },
+        },
+      },
+
+      file_size = {
+        enabled = false,
+      },
+      created = {
+        enabled = false,
+      },
+    }
+  end,
 }
