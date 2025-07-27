f6dbad6a461461a1755faf7eb0c4cca030ec743a
werte <juanmanuelveleztorres@gmail.com>
2025-07-28 00:32:18 +0200

    add prittier to mason installed

--- init.lua
+++ init.lua
@@ -484,10 +484,16 @@ require('lazy').setup({
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
-      -- Mason must be loaded before its dependents so we need to set it up here.
-      -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'mason-org/mason.nvim', opts = {} },
+      {
+        'mason-org/mason.nvim',
+        opts = {
+          ensure_installed = {
+            'prettier',
+          },
+          auto_update = true,
+          run_on_start = true,
+        },
+      },
       'mason-org/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
